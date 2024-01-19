f2f5217cb1de466f8d45182076c55aaa52e5cc89
tup80767 <tup80767@temple.edu>
2024-01-18 23:38:57 -0500

    added files

---
+++ random.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "random.h"
+
+int main()
+{
+	int a;
+
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
+
+	printf("Today's random word: ");
+	for(a=0;a<7;a++)
+		putchar( randchar() );
+	putchar('\n');
+	
+
+	return(0);
+}
+
+char randchar(){
+    int randNum =  97 + (rand() % 25);
+    return (char) randNum;
+}
